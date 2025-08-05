ead3f9555206135b135a8941540dc56da6b0800e
aimisyafiqa <syafiqafdr@gmail.com>
2025-08-05 14:43:13 +0800

    updated login

--- ideabajet/models.py
+++ ideabajet/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Admin(models.Model):
+    adminid = models.CharField(max_length=10, primary_key=True)
+    password = models.CharField(max_length=20)
+    name = models.TextField()
+
--- ideabajet/views.py
+++ ideabajet/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Admin 
 # Create your views here.
 
 def login(request):
+    if request.method == 'POST':
+        adminid = request.POST.get('id')
+        password = request.POST.get('password')
+
+        try:
+            admin = Admin.objects.get(adminid=adminid, password=password)
+            request.session['id'] = admin.adminid  # simpan dalam session
+            messages.success(request, "Succesfully Login.", extra_tags='admin')
+            return render(request,'dashboard.html', {'message': 'Sucessfully Login'})  # ganti dengan nama URL dashboard admin awak
+        except Admin.DoesNotExist:
+            return render(request, 'login.html', {'message': 'Invalid ID or IC number',})
     return render (request,"login.html")
+
+def dashboard(request):
+    return render(request,"dashboard.html")
+

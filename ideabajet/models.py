9370447247fec3a70055dd911b5f413c59e542d3
aimisyafiqa <syafiqafdr@gmail.com>
2025-08-06 11:13:10 +0800

    syafiqa

--- ideabajet/models.py
+++ ideabajet/models.py
@@ -10,6 +10,14 @@ class Lokasi(models.Model):
     lid = models.AutoField(primary_key=True)
     list_lokasi = models.CharField(max_length=255)
 
+class Aset(models.Model):
+    asetid = models.AutoField(primary_key=True)
+    list_aset = models.CharField(max_length=255)
+
+class Elemen8(models.Model):
+    asetid = models.AutoField(primary_key=True)
+    list_aset = models.TextField()
+
 class Elemen1(models.Model):
     e1id = models.AutoField(primary_key=True)
     e1 = models.TextField()
@@ -25,3 +33,19 @@ class Elemen3(models.Model):
 class Elemen4(models.Model):
     e4id = models.AutoField(primary_key=True)
     e4 = models.TextField()
+
+class Elemen5(models.Model):
+    e5id = models.AutoField(primary_key=True)
+    e5 = models.TextField()
+
+class Elemen6(models.Model):
+    e6id = models.AutoField(primary_key=True)
+    e6 = models.TextField()
+
+class Elemen7(models.Model):
+    e7id = models.AutoField(primary_key=True)
+    e7 = models.TextField()
+
+class Elemen8(models.Model):
+    e8id = models.AutoField(primary_key=True)
+    e8 = models.TextField()
--- ideabajet/views.py
+++ ideabajet/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from . models import Admin, Elemen1, Lokasi, Elemen2, Elemen3, Elemen4
+from . models import Admin, Elemen1, Lokasi, Elemen2, Elemen3, Elemen4, Elemen5, Elemen6, Elemen7, Elemen8, Aset
 # Create your views here.
 
 def login(request):
@@ -11,34 +11,14 @@ def login(request):
         try:
             admin = Admin.objects.get(adminid=adminid, password=password)
             request.session['id'] = admin.adminid  # simpan dalam session
-            messages.success(request, "Succesfully Login.", extra_tags='admin')
+            messages.success(request, "Succesfully Login.", extra_tags='user')
             return render(request,'dashboard.html', {'message': 'Sucessfully Login'})  # ganti dengan nama URL dashboard admin awak
         except Admin.DoesNotExist:
-            return render(request, 'login.html', {'message': 'Invalid Admin ID or Password',})
+            return render(request, 'login.html', {'message': 'Invalid Admin ID or Password'}, extra_tags='user')
     return render (request,"login.html")
 
 def dashboard(request):
     return render(request,"dashboard.html")
-
-def lokasi(request):
-    if request.method == 'POST':
-        listloc = request.POST.get('list_lokasi')
-        if listloc:
-            Lokasi.objects.create(list_lokasi=listloc)
-            return redirect('lokasi')
-    data = Lokasi.objects.all()
-    return render(request, 'lokasi.html', {'data': data})
-
-def delete_lokasi(request, lid):
-    Lokasi.objects.filter(lid=lid).delete()
-    return redirect('lokasi')
-
-def edit_lokasi(request, lid):
-    if request.method == 'POST':
-        listloc = request.POST.get('list_lokasi')
-        if listloc:
-            Lokasi.objects.filter(lid=lid).update(list_lokasi=listloc)
-    return redirect('lokasi')
 
 def e1(request):
     if request.method == 'POST':
@@ -51,13 +31,6 @@ def e1(request):
 
 def delete_e1(request, e1id):
     Elemen1.objects.filter(e1id=e1id).delete()
-    return redirect('e1')
-
-def edit_e1(request, e1id):
-    if request.method == 'POST':
-        liste1 = request.POST.get('e1')
-        if liste1:
-            Elemen1.objects.filter(e1id=e1id).update(e1=liste1)
     return redirect('e1')
 
 def e2(request):
@@ -73,13 +46,6 @@ def delete_e2(request, e2id):
     Elemen2.objects.filter(e2id=e2id).delete()
     return redirect('e2')
 
-def edit_e2(request, e2id):
-    if request.method == 'POST':
-        value = request.POST.get('e2')
-        if value:
-            Elemen2.objects.filter(e2id=e2id).update(e2=value)
-    return redirect('e2')
-
 def e3(request):
     if request.method == 'POST':
         liste3 = request.POST.get('e3')
@@ -91,13 +57,6 @@ def e3(request):
 
 def delete_e3(request, e3id):
     Elemen3.objects.filter(e3id=e3id).delete()
-    return redirect('e3')
-
-def edit_e3(request, e3id):
-    if request.method == 'POST':
-        value = request.POST.get('e3')
-        if value:
-            Elemen3.objects.filter(e3id=e3id).update(e3=value)
     return redirect('e3')
 
 def e4(request):
@@ -113,9 +72,80 @@ def delete_e4(request, e4id):
     Elemen4.objects.filter(e4id=e4id).delete()
     return redirect('e4')
 
-def edit_e4(request, e4id):
+def e5(request):
     if request.method == 'POST':
-        value = request.POST.get('e4')
-        if value:
-            Elemen4.objects.filter(e4id=e4id).update(e4=value)
-    return redirect('e4')
+        liste5 = request.POST.get('e5')
+        if liste5:
+            Elemen5.objects.create(e5=liste5)
+            return redirect('e5')
+    data = Elemen5.objects.all()
+    return render(request, 'e5.html', {'data': data})
+
+def delete_e5(request, e5id):
+    Elemen5.objects.filter(e5id=e5id).delete()
+    return redirect('e5')
+
+def e6(request):
+    if request.method == 'POST':
+        liste6 = request.POST.get('e6')
+        if liste6:
+            Elemen6.objects.create(e6=liste6)
+            return redirect('e6')
+    data = Elemen6.objects.all()
+    return render(request, 'e6.html', {'data': data})
+
+def delete_e6(request, e6id):
+    Elemen6.objects.filter(e6id=e6id).delete()
+    return redirect('e6')
+
+def e7(request):
+    if request.method == 'POST':
+        liste7 = request.POST.get('e7')
+        if liste7:
+            Elemen7.objects.create(e7=liste7)
+            return redirect('e7')
+    data = Elemen7.objects.all()
+    return render(request, 'e7.html', {'data': data})
+
+def delete_e7(request, e7id):
+    Elemen7.objects.filter(e7id=e7id).delete()
+    return redirect('e7')
+
+def e8(request):
+    if request.method == 'POST':
+        liste8 = request.POST.get('e8')
+        if liste8:
+            Elemen8.objects.create(e8=liste8)
+            return redirect('e8')
+    data = Elemen8.objects.all()
+    return render(request, 'e8.html', {'data': data})
+
+def delete_e8(request, e8id):
+    Elemen8.objects.filter(e8id=e8id).delete()
+    return redirect('e8')
+
+def lokasi(request):
+    if request.method == 'POST':
+        listloc = request.POST.get('list_lokasi')
+        if listloc:
+            Lokasi.objects.create(list_lokasi=listloc)
+            return redirect('lokasi')
+    data = Lokasi.objects.all()
+    return render(request, 'lokasi.html', {'data': data})
+
+def delete_lokasi(request, lid):
+    Lokasi.objects.filter(lid=lid).delete()
+    return redirect('lokasi')
+
+def aset(request):
+    if request.method == 'POST':
+        listaset = request.POST.get('list_aset')
+        if listaset:
+            Aset.objects.create(list_aset=listaset)
+            return redirect('aset')
+    data = Aset.objects.all()
+    return render(request, 'aset.html', {'data': data})
+
+def delete_aset(request, asetid):
+    Aset.objects.filter(asetid=asetid).delete()
+    return redirect('aset')
